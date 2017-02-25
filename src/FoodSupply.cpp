74ba4a45dddcfd02702bdc5bf083d1cbd285e217
Andrei Vasilev <andreisvasilev@gmail.com>
2017-02-24 16:50:16 -0800

    Update FoodSupply.cpp

--- src/FoodSupply.cpp
+++ src/FoodSupply.cpp
@@ -18,7 +18,7 @@ namespace BC {
         double chance = chance_dist(rd);
 
         // Food has a 0.5% chance of being generated
-        if(chance < 0.005 && m_food_array.size() < MAX_FOOD_COUNT) {
+        if(chance < 0.005 && m_food_array.size() <= MAX_FOOD_COUNT) {
             Food food(m_environment_width, m_environment_height);
             m_food_array.push_back(food);
         }
